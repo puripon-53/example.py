c2be3c72a43db06a1c0dddfcc70dac1dffac81a9
puripon53 <yy.schist@gmail.com>
2025-10-08 18:18:47 +0900

    ハッカソンの課題

---
+++ example.py
@@ -0,0 +1,19 @@
+print('hello world')
+
+def greet():
+    print('こんにちは')
+greet()
+
+def print_name(name):
+    print(f'私の名前は{name}です')
+print_name('名前')
+
+def get_greet():
+    return 'おはようございます'
+print(get_greet())
+
+def add(a, b):
+    return a + b
+answer = add(7, 9)
+print(answer)
+
